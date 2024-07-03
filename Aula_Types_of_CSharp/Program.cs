276d27e2a29065085779c5efb2330a3773cb9f77
Matheus Henrique Almeida Vieira Neves <matheus16092004@gmail.com>
2024-07-03 20:51:24 -0300

    Adicionar arquivos de projeto.

---
+++ Aula_Types_of_CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//Tipos de variáveis em C# eles variam entre 2 tipos os com sinal e os sem sinal o que muda o valor máximo que pode ser armazenado.
+SByte sbyteTeste = 127;//sbyte é um tipo de variável com sinal que pode armazenar valores de -128 a 127.
+Byte byteTeste = 255;//byte é um tipo de variável sem sinal que pode armazenar valores de 0 a 255.
+short shortTeste = 32767;//short é um tipo de variável com sinal que pode armazenar valores de -32768 a 32767.
+ushort ushortTeste = 65535;//ushort é um tipo de variável sem sinal que pode armazenar valores de 0 a 65535.
+int intTeste = 2147483647;//int é um tipo de variável com sinal que pode armazenar valores de -2147483648 a 2147483647.
+uint uintTeste = 4294967295;//uint é um tipo de variável sem sinal que pode armazenar valores de 0 a 4294967295.
+long longTeste = 9223372036854775807;//long é um tipo de variável com sinal que pode armazenar valores de -9223372036854775808 a 9223372036854775807.
+ulong ulongTeste = 18446744073709551615;//ulong é um tipo de variável sem sinal que pode armazenar valores de 0 a 18446744073709551615.
+float floatTeste = 3.40282347E+38F;//float é um tipo de variável com sinal que pode armazenar valores de -3.40282347E+38F a 3.40282347E+38F.
+double doubleTeste = 1.7976931348623157E+308;//double é um tipo de variável com sinal que pode armazenar valores de -1.7976931348623157E+308 a 1.7976931348623157E+308.
+string stringTeste = "Teste";//string é um tipo de variável que armazena texto.
+char charTeste = 'T';//char é um tipo de variável que armazena um caractere.
+bool boolTeste = true;//bool é um tipo de variável que armazena um valor booleano. Pode ser true ou false.
